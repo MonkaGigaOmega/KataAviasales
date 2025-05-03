9abd201b48d1477d5c6879ccc9c4ddc0e1a8e98b
Gosha Shestakov <gosha3103@mail.ru>
2025-05-03 14:14:59 +0300

    удалил девтулс из-за версела

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,7 +1,6 @@
 import { createStore, applyMiddleware } from 'redux'
 import rootReducer from './reducers/rootReducer'
 import { thunk } from 'redux-thunk'
-import { composeWithDevTools } from 'redux-devtools-extension'
-const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)))
+const store = createStore(rootReducer, applyMiddleware(thunk))
 
 export default store
